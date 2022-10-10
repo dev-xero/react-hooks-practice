a93919aeeba3c18c50b066b96612f1d651f1a39e
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 13:26:21 +0100

    Initialized Project

---
+++ src/components/SongList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SongList = () => {
+	return (
+		<div className="song-list">
+			<ul>
+				<li>Cry Baby - Official DiSM</li>
+				<li>Blue Bird - Akano</li>
+			</ul>
+		</div>
+	)
+}
+
+export default SongList
