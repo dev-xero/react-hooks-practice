3ae1d63772c69f4c3cfc0cb9c993ed34babd2f77
CodeNinja-tech <elo48858@gmail.com>
2022-10-10 13:29:49 +0100

    Updated SongList Component

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const SongList = () => {
 	return (
-		<div className="song-list">
-			<ul>
-				<li>Cry Baby - Official DiSM</li>
-				<li>Blue Bird - Akano</li>
-			</ul>
-		</div>
-	)
+    <div className="song-list">
+      <ul>
+        <li>Cry Baby - Official Hige Dandism</li>
+        <li>Blue Bird - Ikimono-Gakari</li>
+      </ul>
+    </div>
+  )
 }
 
 export default SongList
